aae62ec5b7bb8b3dbd26dfe7030e96e97329dd41
walter-grace <nico.zahniser@gmail.com>
2021-03-06 09:03:26 -0800

    few changes

--- src/features/spotifyExample/SpotifyExample.tsx
+++ src/features/spotifyExample/SpotifyExample.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectDisplayName, selectProduct, selectExternalUrL} from './spotifyExampleSlice';
 import styles from './SpotifyExample.module.css';
 import Love from './Text'
 import { getHashParams, removeHashParamsFromUrl } from '../../utils/hashUtils';
 import { setUserProfileAsync } from '../spotifyExample/spotifyExampleSlice';
+import axios from 'axios';
 
 
 
 export function SpotifyExample() {
+
   const displayName = useSelector(selectDisplayName);
   const product = useSelector(selectProduct);
-  
+  const [name, setName] = useState('');
+	const [age, setAge] = useState('');
+	const [artist, setArtist] = useState('');
+	const [hobby, setHobby] = useState('');
+	const [spotify, setSpotify] = useState('');
+ 
   return (
     <div className={styles.column}>
       <Love/>
       {displayName && <div className={styles.row}>
-        Welcome!! 🎶♥️ {displayName}
+        <span>
+        Welcome!! 🎶♥️ {displayName}  
+          </span> 
       </div>}
       {product && <div className={styles.row}>
         
@@ -36,8 +45,16 @@ export function SpotifyExample() {
           <button
           className={styles.button}
           aria-label="Log in using OAuth 2.0"
-          onClick={() => window.open("https://open.spotify.com/user/" + displayName)}  >
+          onClick={() => window.open("https://compassionate-data.netlify.app/")}  >
           Match Me
+          </button>
+          <hr />
+          <br />
+          <button
+          className={styles.button}
+          aria-label="Log in using OAuth 2.0"
+          onClick={() => window.open("https://6043b2afa5d98f55c8a2c073--objective-bardeen-5534ad.netlify.app")}  >
+          Prototype
           </button>  
           </div>
       </div>
